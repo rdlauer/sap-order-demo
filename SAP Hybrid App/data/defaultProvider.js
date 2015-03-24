cc75704902c3d8166d533fa2a7281f2de94ac3f8
Rob Lauer <rob.lauer@telerik.com>
2015-03-24 16:01:01 +0000

    Changes from Screen Builder

---
+++ SAP Hybrid App/data/defaultProvider.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function() {
+    //create a reference for the data provider to be used throughout the app
+
+    app.data.defaultprovider = new Everlive({
+        apiKey: 'SYKvpaE6gzcrAuag',
+        scheme: 'https'
+    });
+
+}());
